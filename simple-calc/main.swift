3c27da2ba65f46996180d28c138c25639a6b46a0
Henry Zhang <henry.zhang@live.com>
2016-10-04 19:15:27 -0700

    first commit

---
+++ simple-calc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simple-calc
+//
+//  Created by iGuest on 10/4/16.
+//  Copyright © 2016 henryylzhang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
