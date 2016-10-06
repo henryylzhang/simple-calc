54aa9e0dde8d32a4c15f3c7191a73519e11ef8dc
Student User <studentuser@Students-MacBook-Pro.local>
2016-10-06 02:26:27 -0700

    create basic arithmetic functions

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 #simple-calc
 
-A simple command line calculator that supports basic arithmatic, factorial, average, and counting commands.
+A simple command line calculator that supports basic arithmetic, factorial, average, and counting commands.
+
+Examples:
+> 5 fact
+= 120
+
+> 2 4 6 8 10 avg
+= 6
+
+>4 1 5 631 21 count
+= 5
--- simple-calc/main.swift
+++ simple-calc/main.swift
@@ -10,22 +10,37 @@ import Foundation
 
 print("Enter an expression separated by returns: \n")
 var response = readLine(strippingNewline: true)!
-let num1 = UInt.init(response)!
-response = readLine(strippingNewline: true)!
-var operate = ""
-switch operate {
-    case "+":
-        operate = "+"
-    case "-":
-        operate = "-"
-    case "*":
-        operate = "*"
-    case "/":
-        operate = "/"
-    case "%":
-        operate = "%"
-    default:
-        print ("Please enter a valid operator")
+if response.contains("fact") {
+    let container: [String] = response.characters.split{$0 == " "}.map(String.init)
+} else if response.contains("avg") {
+    
+} else if response.contains("count") {
+    
+} else {
+//let num1 = UInt.init(response)!
+//response = readLine(strippingNewline: true)!
+//if response == "+" {
+//    let ans = num1 + UInt.init(readLine(strippingNewline: true)!)!
+//    print("Result: \(ans)")
+//} else if response == "-" {
+//    let ans = num1 - UInt.init(readLine(strippingNewline: true)!)!
+//    print("Result: \(ans)")
+//} else if response == "*" {
+//    let ans = num1 * UInt.init(readLine(strippingNewline: true)!)!
+//    print("Result: \(ans)")
+//} else if response == "/" {
+//    let ans = num1 * UInt.init(readLine(strippingNewline: true)!)!
+//    print("Result: \(ans)")
+//} else if response == "%" {
+//    let ans = num1 % UInt.init(readLine(strippingNewline: true)!)!
+//    print("Result: \(ans)")
+//}
 }
 
-//if response = readLine()
+//func factorial(number: Int) -> Int {
+//    if number <= 1 {
+//        return 1;
+//    } else {
+//        return Int(number * factorial(number - 1))
+//    }
+//}
